5b0a76d460bab0a0aadd063392c3a056008a63bf
Finn Martin <finnmartin2424@gmail.com>
2025-01-23 19:33:14 +0000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9065824f-4973-49df-95c0-17c49a7b64d2';
+export const MODAL_CLOSE_SEC = 2.5;
